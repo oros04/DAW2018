3280e1f464dc66398f3018909eb91619a9c6e985
Mario Soro Gomez <msoro@saytel.es>
2018-04-17 19:59:52 +0200

    Presentacion Terminada-Bug en contactos buscar y falta ficha peliculas

---
+++ MSoro-AragonSkills/Controllers/PeliculasController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSoro_AragonSkills.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
